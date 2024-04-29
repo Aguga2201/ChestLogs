324a9f9e92af1c797100cfc6e71af1485a14c43f
simon <simon.meyer@mein.gmx>
2024-04-29 20:52:52 +0200

    Initial commit

---
+++ src/main/java/com/aguga/ChestLog.java
@@ -0,0 +1,109 @@
+package com.aguga;
+
+import com.aguga.Utils.Utils;
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.WorldSavePath;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ChestLog implements ModInitializer
+{
+    public static final Logger LOGGER = LoggerFactory.getLogger("chest-log");
+	String folderPath;
+
+	@Override
+	public void onInitialize()
+	{
+		ServerLifecycleEvents.SERVER_STARTED.register((server ->
+		{
+			folderPath = server.getSavePath(WorldSavePath.ROOT).resolve("ChestLog").toString();
+			File folder = new File(folderPath);
+
+			if (!folder.exists())
+			{
+				boolean created = folder.mkdirs();
+				if (!created)
+					LOGGER.error("Failed to create folder: " + folderPath);
+			}
+		}));
+
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) ->
+		{
+			Block block = world.getBlockState(hitResult.getBlockPos()).getBlock();
+			BlockEntity blockEntity = world.getBlockEntity(hitResult.getBlockPos());
+
+			List<Block> chestBlocks = Arrays.asList(Blocks.CHEST, Blocks.TRAPPED_CHEST, Blocks.BARREL, Blocks.SHULKER_BOX);
+			if(!chestBlocks.contains(block))
+				return ActionResult.PASS;
+
+			String blockstr = block.getName().getString();
+			BlockPos pos = hitResult.getBlockPos();
+
+			AtomicReference<String> timeStamp = new AtomicReference<>(Utils.getTimeStamp());
+			String dimStr = Utils.getDimString(world.getDimensionKey());
+
+			String savePath = folderPath + File.separator + blockstr + " " + pos.getX() + " " + pos.getY() + " " + pos.getZ() + " " + dimStr;
+
+			List<ItemStack> itemsBefore = List.copyOf(Utils.getItems(blockEntity));
+			List<String> itemsBeforeStr = Utils.itemStackListToStrList(itemsBefore);
+
+			CompletableFuture<Void> chestClosed = CompletableFuture.runAsync(() ->
+			{
+				try
+				{
+					Thread.sleep(2000);
+				} catch (InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+
+				Vec3d initialPos = player.getPos();
+				Float initialYaw = player.getYaw();
+				Float initialPitch = player.getPitch();
+
+				Float currentYaw = player.getYaw();
+				Float currentPitch = player.getPitch();
+				Vec3d currentPos = player.getPos();
+
+				while(initialYaw.equals(currentYaw) && initialPitch.equals(currentPitch) && initialPos.equals(currentPos))
+				{
+					try
+					{
+						Thread.sleep(400);
+					} catch (InterruptedException e)
+					{
+						e.printStackTrace();
+					}
+					currentPos = player.getPos();
+					currentYaw = player.getYaw();
+					currentPitch = player.getPitch();
+				}
+
+				List<ItemStack> itemsAfter = Utils.getItems(blockEntity);
+				List<String> itemsAfterStr = Utils.itemStackListToStrList(itemsAfter);
+				timeStamp.set(Utils.getTimeStamp());
+
+				List<String> itemsAdded = Utils.getChangedItems(itemsBeforeStr, itemsAfterStr);
+				List<String> itemsRemoved = Utils.getChangedItems(itemsAfterStr, itemsBeforeStr);
+
+				Utils.writeLog(timeStamp.get(), player.getDisplayName().getString(), savePath, itemsAdded, itemsRemoved);
+			});
+			return ActionResult.PASS;
+		});
+	}
+}
---
+++ src/main/java/com/aguga/Utils/Utils.java
@@ -0,0 +1,178 @@
+package com.aguga.Utils;
+
+import com.aguga.ChestLog;
+import net.minecraft.block.entity.*;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.dimension.DimensionTypes;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Utils
+{
+    public static String getTimeStamp()
+    {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yy  HH:mm:ss");
+        return String.format("[%s]", dtf.format(LocalDateTime.now()));
+    }
+
+    public static String getDimString(RegistryKey<DimensionType> dimensionType)
+    {
+        String dimStr = "unkown";
+        if(dimensionType == DimensionTypes.OVERWORLD)
+            dimStr = "Overworld";
+        else if (dimensionType == DimensionTypes.THE_NETHER)
+            dimStr = "Nether";
+        else if (dimensionType == DimensionTypes.THE_END)
+            dimStr = "The End";
+        return dimStr;
+    }
+
+    public static List<ItemStack> getItems(BlockEntity blockEntity)
+    {
+        List<ItemStack> itemStacks = new ArrayList<>();
+
+        if(blockEntity instanceof ChestBlockEntity)
+        {
+            ChestBlockEntity chestEntity = (ChestBlockEntity) blockEntity;
+            for(int i = 0; i < chestEntity.size(); i++)
+            {
+                ItemStack itemStack = chestEntity.getStack(i);
+                if(!itemStack.isEmpty())
+                {
+                    itemStacks.add(itemStack);
+                }
+            }
+        }
+
+        if(blockEntity instanceof BarrelBlockEntity)
+        {
+            BarrelBlockEntity barrelEntity = (BarrelBlockEntity) blockEntity;
+            for(int i = 0; i < barrelEntity.size(); i++)
+            {
+                ItemStack itemStack = barrelEntity.getStack(i);
+                if(!itemStack.isEmpty())
+                {
+                    itemStacks.add(itemStack);
+                }
+            }
+        }
+
+        if(blockEntity instanceof ShulkerBoxBlockEntity)
+        {
+            ShulkerBoxBlockEntity shulkerBoxBlockEntity = (ShulkerBoxBlockEntity) blockEntity;
+            for(int i = 0; i < shulkerBoxBlockEntity.size(); i++)
+            {
+                ItemStack itemStack = shulkerBoxBlockEntity.getStack(i);
+                if(!itemStack.isEmpty())
+                {
+                    itemStacks.add(itemStack);
+                }
+            }
+        }
+
+        if(blockEntity instanceof TrappedChestBlockEntity)
+        {
+            TrappedChestBlockEntity trappedChestBlockEntity = (TrappedChestBlockEntity) blockEntity;
+            for(int i = 0; i < trappedChestBlockEntity.size(); i++)
+            {
+                ItemStack itemStack = trappedChestBlockEntity.getStack(i);
+                if(!itemStack.isEmpty())
+                {
+                    itemStacks.add(itemStack);
+                }
+            }
+        }
+
+        return itemStacks;
+    }
+
+    public static void writeLog(String timeStamp, String playerName, String path, List<String> itemsAdded, List<String> itemsRemoved)
+    {
+        try
+        {
+            String logStr;
+
+            logStr = timeStamp + " " + playerName + "\nItems Added: " + itemsAdded + "\nItems Removed: " + itemsRemoved + "\n\n";
+
+            FileWriter fileWriter = new FileWriter(new File(path), true);
+            fileWriter.write(logStr);
+            fileWriter.close();
+        } catch (IOException e)
+        {
+            ChestLog.LOGGER.error("Error writing log");
+            e.printStackTrace();
+        }
+    }
+
+    public static List<String> itemStackListToStrList(List<ItemStack> items)
+    {
+        String itemsStr = items.toString();
+        List<String> itemsList = Arrays.asList(itemsStr.substring(1, itemsStr.length() - 1).split(","));
+        List<String> outputList = new ArrayList<>();
+
+        for (String item : itemsList) {
+            if(item.isEmpty()) continue;
+
+            String[] parts = item.trim().split(" ");
+            if(parts.length < 2) continue;
+
+            String countStr = parts[0];
+            String type = parts[1];
+
+            outputList.add(countStr + " " + type);
+        }
+        return outputList;
+    }
+
+    public static List<String> getChangedItems(List<String> list1, List<String> list2)
+    {
+        List<String> outputList = new ArrayList<>();
+        List<String> done = new ArrayList<>();
+
+        Integer count;
+        Integer count1;
+        Integer count2;
+
+        for(String item : list2)
+        {
+            if(done.contains(item.split(" ", 2)[1]))
+                continue;
+
+            count = 0;
+            count1 = 0;
+            count2 = 0;
+            for(String str : list2)
+            {
+                if(str.split(" ", 2)[1].equals(item.split(" ", 2)[1]))
+                {
+                    count1 += Integer.parseInt(str.split(" ")[0]);
+                }
+            }
+
+            for(String str : list1)
+            {
+                if(str.split(" ", 2)[1].equals(item.split(" ", 2)[1]))
+                {
+                    count2 += Integer.parseInt(str.split(" ")[0]);
+                }
+            }
+
+            count = count1 - count2;
+            if(count > 0)
+            {
+                outputList.add(outputList.size(), count + " " + item.split(" ", 2)[1]);
+            }
+            done.add(item.split(" ", 2)[1]);
+        }
+        return outputList;
+    }
+}
